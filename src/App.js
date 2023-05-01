dd2f6ce78e4811bc884c7e9ab4c6305981db6783
seheeeJeong <usung7888@naver.com>
2023-05-01 16:21:01 +0900

    study: react redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// redux 미적용
+
+import { useState } from "react";
+import "./styles/Box.css";
 
 function App() {
+  const [number, setNumber] = useState(100);
+
+  const onIncrease = () => {
+    setNumber(number + 1);
+  };
+
+  const onDecrease = () => {
+    setNumber(number - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Redux Test
+      <Box1 number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
     </div>
   );
 }
+
+const Box1 = ({ number, onIncrease, onDecrease }) => {
+  return (
+    <div className="Box">
+      <h2>Box1: {number}</h2>
+      <Box2 number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    </div>
+  );
+};
+
+const Box2 = ({ number, onIncrease, onDecrease }) => {
+  return (
+    <div className="Box">
+      <h2>Box2: </h2>
+      <Box3 number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    </div>
+  );
+};
+
+const Box3 = ({ number, onIncrease, onDecrease }) => {
+  return (
+    <div className="Box">
+      <h2>Box3: {number} </h2>
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import App2 from "./App2";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// state 초기값 설정
+const initialState = {
+  number: 9,
+};
+
+// Reducer 정의: 변화를 일으키는 함수
+// dispatch가 실행됐을 때 호출되는 함수
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        number: state.number + 1,
+      };
+    case "DECREMENT":
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// store 정의: 상태를 관리하는 공간! 단, 하나의 프로젝트에는 하나의 store만 있어야 함
+const store = configureStore({ reducer: reducer }, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Provider 컴포넌트: store props를 반드시 가져야 함
+  // Provider 하위 컴포넌트들은 store에 저장된 state들을 사용 가능!
+  <Provider store={store}>
+    <App2 />
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
